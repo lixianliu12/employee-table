3aafc0c444bf5c307af81e3430f80eb1787dbd3f
lixianyorku <59426061+lixianyorku@users.noreply.github.com>
2023-04-06 22:48:30 -0400

    add employee table

--- frontend/src/Employee.js
+++ frontend/src/Employee.js
@@ -1,9 +1,28 @@
-import React from 'react'
-
+import React, { useEffect } from 'react'
+import axios from 'axios'
 function Employee() {
+    useEffect(() => {
+        axios.get('http://localhost:8081/')
+        .then(res => console.log(res))
+        .catch(err => console.log(err));
+    }, [])
+
   return (
-    <div>
-      
+    <div className='d-flex vh-100 bg-primary justify-content-center align-items-center'>
+      <div className='w-50 bg-white rounded'>
+        <button className='btn btn-success'>Add +</button>
+        <table className='table'>
+            <thead>
+                <tr>
+                    <th>First name</th>
+                    <th>Last name</th>
+                    <th>Salary</th>
+                </tr>
+                
+
+            </thead>
+        </table>
+      </div>
     </div>
   )
 }
