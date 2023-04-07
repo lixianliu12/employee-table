e32987e793a2945df2b61aada61f9f8324988731
lixianyorku <59426061+lixianyorku@users.noreply.github.com>
2023-04-06 23:07:39 -0400

    add action buttons

--- frontend/src/Employee.js
+++ frontend/src/Employee.js
@@ -18,6 +18,7 @@ function Employee() {
                     <th>First name</th>
                     <th>Last name</th>
                     <th>Salary</th>
+                    <th>Action</th>
                 </tr>
                 
 
@@ -29,6 +30,10 @@ function Employee() {
                             <td>{data.Firstname}</td>
                             <td>{data.Lastname}</td>
                             <td>{data.Salary}</td>
+                            <td>
+                                <button className='btn btn-primary'>Edit</button>
+                                <button className='btn btn-danger ms-2'>Delete</button>
+                            </td>
                         </tr>
                     ))
                 }
