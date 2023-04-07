6456e8faebc09ede0d326a3b265a8d6b6abfc3aa
lixianyorku <59426061+lixianyorku@users.noreply.github.com>
2023-04-06 23:02:45 -0400

    connect table content to database

--- frontend/src/Employee.js
+++ frontend/src/Employee.js
@@ -1,15 +1,16 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 function Employee() {
+    const [employee, setEmployee] = useState([])
     useEffect(() => {
         axios.get('http://localhost:8081/')
-        .then(res => console.log(res))
+        .then(res => setEmployee(res.data))
         .catch(err => console.log(err));
     }, [])
 
   return (
     <div className='d-flex vh-100 bg-primary justify-content-center align-items-center'>
-      <div className='w-50 bg-white rounded'>
+      <div className='w-50 bg-white rounded p-3'>
         <button className='btn btn-success'>Add +</button>
         <table className='table'>
             <thead>
@@ -21,6 +22,17 @@ function Employee() {
                 
 
             </thead>
+            <tbody>
+                {
+                    employee.map((data, i)=>(
+                        <tr key={i}>
+                            <td>{data.Firstname}</td>
+                            <td>{data.Lastname}</td>
+                            <td>{data.Salary}</td>
+                        </tr>
+                    ))
+                }
+            </tbody>
         </table>
       </div>
     </div>
