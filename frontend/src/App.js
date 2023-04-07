e9aded4913ad089e8bdcc95a857d23dc52d06656
lixianyorku <59426061+lixianyorku@users.noreply.github.com>
2023-04-06 22:47:51 -0400

    fix package error

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 
-import './App.css';
+
 import 'bootstrap/dist/css/bootstrap.min.css'
-import {BrowserRouter, Routes,Route} from 'react-router-dom'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Employee from './Employee';
 
 function App() {
