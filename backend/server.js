810e251d9a635ccd37514868478d7381e57b0ac1
lixianyorku <59426061+lixianyorku@users.noreply.github.com>
2023-04-07 01:44:27 -0400

    create delete logic backend

--- backend/server.js
+++ backend/server.js
@@ -50,6 +50,17 @@ app.put('/update/:id', (req, res) => {
     })
 })
 
+app.delete('/employee/:id', (req, res) => {
+    const sql = "DELETE FROM employees WHERE ID = ?";
+    
+    const id = req.params.id;
+
+    db.query(sql, [id], (err, data) =>{
+        if(err) return res.json("Error");
+        return res.json(data);
+    })
+})
+
 app.listen(8081, () =>{
     console.log("listening");
 })
